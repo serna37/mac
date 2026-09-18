9fc627d6d59640614972a33ee9711af015623de7
さーな <95469388+serna37@users.noreply.github.com>
2026-09-18 15:42:10 +0900

    Add setup script for Mac development environment
    This script sets up a portable development environment on Mac, installing necessary tools and configuring system settings.

---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Mac用のセットアップ
+# 作業環境をCodespaces上で完結させる前提の、ポータブル構成のため
+# Mac上にはプログラミングに必要なツールセットは導入せず
+# GUI系のユーティリティツールのみを導入する
+
+# =====================================
+# 1. Homebrewのセットアップ
+# =====================================
+# Macならsudo実行のため対話、codespaces中ではnon interactiveで実行
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# =====================================
+# 2. パッケージのインストール
+# =====================================
+REPOS=(
+vim # 魂なので入れざるを得ない
+git # さすがに必要な時がある
+sqlite # 一時的にDB作りたい場合に便利
+)
+CASK_REPOS=(
+vorssaint # ツールセット。クリップボード、スクショ、一時メモなど
+google-drive # ドライブのマウント
+dbeaver-community # DB操作
+another-redis-desktop-manager # Redis操作
+)
+MAS_IDS=(
+1429033973 # RunCat
+)
+for v in ${REPOS[@]}; do
+  brew reinstall $v
+done
+for v in ${CASK_REPOS[@]}; do
+  brew reinstall --cask $v
+done
+brew reinstall mas
+for v in ${MAS_IDS[@]}; do
+  mas install $v
+done
+
+# =====================================
+# 3. フォントを導入
+# =====================================
+# ~/tmp-{yyyyMMddHHmmss}-{0〜32767の乱数}
+export TMP_DIR="$HOME/tmp-$(date +'%Y%m%d%H%M%S')-$RANDOM"
+mkdir -p "$TMP_DIR"
+
+# fontを入れる 三角のやつ
+cd $TMP_DIR
+git clone --depth 1 https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+
+# fontを入れる icon系
+cd $TMP_DIR
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
+
+# 削除
+rm -rf $TMP_DIR
+
+# =====================================
+# 3. Mac固有設定
+# =====================================
+# Finderのキルを有効化
+defaults write com.apple.Finder QuitMenuItem -boolean true
+
+# Finderが.DS_sotreを作らない
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Finderがデフォルトで隠しファイルを表示
+defaults write com.apple.finder AppleShowAllFiles -bool true
